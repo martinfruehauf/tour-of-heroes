c8c2f1e8d00f04b075470fe146719a5d852c8088
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-05-27 15:09:43 +0200

    Add messages

---
+++ src/app/message.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { MessagesService } from './message.service';
+
+describe('MessagesService', () => {
+  let service: MessagesService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(MessagesService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
