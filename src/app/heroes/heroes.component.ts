322ec62ef7c3140f0987b0fea67b74f852aee3bf
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-05-27 14:42:03 +0200

    Add hero service (with incorrect synchronous design)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES} from '../mock-heroes';
+import { HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
 
   public selectedHero: Hero;
   public onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
 }
