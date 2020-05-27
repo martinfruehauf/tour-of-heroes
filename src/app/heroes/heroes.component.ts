e0cceb80049f5d665b98117341a539b4a1066f86
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-05-27 11:59:38 +0200

    Add click event hanlder

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,6 +12,11 @@ export class HeroesComponent implements OnInit {
 
   constructor() { }
 
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   public ngOnInit(): void {
   }
 
