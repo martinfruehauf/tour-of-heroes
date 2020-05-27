743cf0cde236da8e9b8922a84f54beeb74de0008
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-05-27 16:04:20 +0200

    Remove errors

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,12 +19,12 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
+  public onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
 
-  getHeroes(): void {
+  private getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
